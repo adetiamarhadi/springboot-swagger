eac197610ee9b9e1c65c35e22f1566e1edba01b3
Adet <adet@Adetias-MacBook-Pro.local>
2025-01-26 11:08:45 +0700

    add tag to give specific naming for our controller

--- src/main/java/com/github/adetiamarhadi/springboot_swagger/controller/OrderController.java
+++ src/main/java/com/github/adetiamarhadi/springboot_swagger/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.github.adetiamarhadi.springboot_swagger.dto.request.OrderRequest;
 import com.github.adetiamarhadi.springboot_swagger.dto.response.OrderResponse;
 import com.github.adetiamarhadi.springboot_swagger.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Order", description = "The Order API")
 @RestController
 @RequestMapping("/v1/orders")
 public class OrderController {
