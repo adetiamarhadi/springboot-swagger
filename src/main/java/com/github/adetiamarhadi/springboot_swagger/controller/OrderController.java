e98832029230845b1ec9d8b812cefab2b95fd8bf
Adet <adet@Adetias-MacBook-Pro.local>
2025-01-26 11:02:15 +0700

    add summary detail for each endpoint

--- src/main/java/com/github/adetiamarhadi/springboot_swagger/controller/OrderController.java
+++ src/main/java/com/github/adetiamarhadi/springboot_swagger/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.github.adetiamarhadi.springboot_swagger.controller;
 import com.github.adetiamarhadi.springboot_swagger.dto.request.OrderRequest;
 import com.github.adetiamarhadi.springboot_swagger.dto.response.OrderResponse;
 import com.github.adetiamarhadi.springboot_swagger.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +22,14 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "Retrieve order detail based on orderId")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrder(@PathVariable String orderId) {
         OrderResponse order = orderService.getOrder(orderId);
         return ResponseEntity.ok(order);
     }
 
+    @Operation(summary = "Submit new order")
     @PostMapping
     public ResponseEntity<String> submitOrder(@RequestBody OrderRequest orderRequest) {
         String orderId = orderService.createOrder(orderRequest);
